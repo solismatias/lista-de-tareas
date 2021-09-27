bc17b858baa01265488b3586a479fe16e2594b5c
solismatias <solisma42@gmail.com>
2021-09-27 19:57:44 -0300

    agregado logica del boton remover y mensaje cuando no hay listas agregadas

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const inputField = document.querySelector(".newtask__input")
 const inputFieldBtn = document.querySelector(".newtask__btn")
 const listDomSection = document.querySelector(".tasks__ul")
 
+
 let taskList = ["makako", "chompa", "perro viejo"] //every time we create a new task, we will save the text here, globally
 
 
@@ -22,6 +23,7 @@ function showTasks(newTaskText) {
     <i class="fas fa-check"></i>
     `
     listDomSection.appendChild(li)
+    emptyTaskList() //
 }
 
 function newTask() {
@@ -40,3 +42,43 @@ inputField.addEventListener('keypress', function (e) { // allow to submit a new 
         newTask()
     }
 })
+
+function emptyTaskList() { // Check if the task list is empty and display a text
+    let textEmpty
+    textEmpty = document.createElement("p")
+    textEmpty.classList.add("tasks__empty-text")
+    textEmpty.innerText = "Estas libre de tareas! Escribe algo para agregarlo a la lista"
+    if (listDomSection.children.length === 0) {
+        listDomSection.appendChild(textEmpty)
+    }
+    else if (listDomSection.children.length > 1) {
+        listDomSection.childNodes.forEach((child) => {
+            if (child.className === "tasks__empty-text") {
+                listDomSection.removeChild(child)
+            }
+        })
+    }
+}
+
+function removeTask(e) {
+    if (e.target.classList.value === 'fas fa-check') {
+        let targetTask = e.target.parentNode
+        listDomSection.removeChild(targetTask)
+    }
+    emptyTaskList()
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    // const taskDoneBtn = [...document.querySelectorAll(".fa-check")].forEach(btn => {
+    //     btn.addEventListener('click', (e) => {
+    //         console.log(e.target.parentNode);
+    //         let targetTask = e.target.parentNode
+    //         listDomSection.removeChild(targetTask)
+    //     })
+    // })
+    // console.log(document.querySelectorAll(".fa-check"));
+    // removeTask()
+})
+
+listDomSection.addEventListener('click', removeTask)
