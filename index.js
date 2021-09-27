54a8c9542212eb146c6f21885db8000d597a1a9b
solismatias <solisma42@gmail.com>
2021-09-26 22:10:43 -0300

    Agregada la logica para mostrar el texto ingresado

---
+++ index.js
@@ -0,0 +1,39 @@
+const inputField = document.querySelector(".newtask__input")
+const inputFieldBtn = document.querySelector(".newtask__btn")
+const listDomSection = document.querySelector(".tasks__ul")
+
+let taskList = ["makako", "chompa", "perro viejo"] //every time we create a new task, we will save the text here, globally
+
+
+function getInputText() {
+    let newTaskText = inputField.value
+    taskList = [...taskList, newTaskText]
+    // debugger
+    // console.log(newTaskText.length);
+    if (newTaskText.trim() !== "") { // with this I check if the string have text or only white space  
+        // return
+        showTasks(newTaskText)
+    }
+}
+
+function showTasks(newTaskText) {
+    let li = document.createElement("li")
+    li.classList.add("tasks__li")
+    li.innerHTML = `
+    <p class="tasks__text">${newTaskText}</p>
+    <i class="fas fa-check"></i>
+    `
+    listDomSection.appendChild(li)
+}
+
+function newTask() {
+    // first we have to save the text of the input field as a string and store it in the taskList
+    getInputText()
+    // iterate the taskList array to create in the DOM all the stored tasks
+    // showTasks()
+}
+
+// 
+inputFieldBtn.addEventListener('click', () => {
+    newTask()
+})
